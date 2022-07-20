fac2e96a232505a40940a8bc9c200e80f2541ee8
Vishnu Vardhan Kumar Pallati <vpallati@outlook.com>
2022-07-20 19:24:42 -0400

    dist changes

--- backend/dist/src/entity/category.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { ProductEnity } from './product.entity';
-export declare class CategoryEntity {
-    id: number;
-    name: string;
-    isActive: boolean;
-    description: string;
-    imagePath: string;
-    products: ProductEnity[];
-}
--- backend/dist/src/entity/product.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryEntity } from './category.entity';
-export declare class ProductEnity {
-    id: number;
-    name: string;
-    isActive: boolean;
-    description: string;
-    imagePaths: string[];
-    category: CategoryEntity;
-}
